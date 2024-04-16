81408703f7cb14b1fa82b19b8ce950c11c1813dd
VaciekMys <mwspolecznosci@gmail.com>
2024-04-16 20:34:29 +0200

    [FE][13] Likes&Dislikes

--- front/VideoBase/src/Components/SearchScreen.js
+++ front/VideoBase/src/Components/SearchScreen.js
@@ -1,14 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
-import {
-  CardHeader,
-  Container,
-  Row,
-  Col,
-  Card,
-  CardText,
-} from "react-bootstrap";
-import styles from "../CSS/searchScreen.module.css";
 
 const SEARCH_QUERY = gql`
   query SearchPosts($keywords: String!) {
@@ -22,6 +13,7 @@ const SEARCH_QUERY = gql`
       }
       video {
         id
+        url
       }
     }
   }
@@ -29,48 +21,62 @@ const SEARCH_QUERY = gql`
 
 function SearchScreen() {
   const { keywords } = useParams();
+  const navigate = useNavigate();
 
   const { data } = useQuery(SEARCH_QUERY, {
     variables: { keywords },
   });
 
+  const redirectToVideo = (post, event) => {
+    event.preventDefault();
+    navigate(`/video/${post.video.id}`, {
+      state: {
+        videoUrl: post.video.url,
+        videoTitle: post.title,
+        videoDescription: post.description,
+        uploaderName: post.user
+          ? post.user.username
+          : "Niezalogowany użytkownik",
+      },
+    });
+  };
+
   return (
-    <Container fluid>
+    <div>
       {data &&
         data.searchPost &&
         data.searchPost.map((post) => (
-          <div className={styles.customCard}>
-            <Row
-              key={post.video.id}
-              className={`${styles.cardRow} mb-4 align-items-stretch`}
+          <div key={post.video.id} className="row mb-4 align-items-stretch ">
+            <div
+              className="col-md-3 px-0  cursor-pointer"
+              onClick={(e) => redirectToVideo(post, e)}
             >
-              <Col md={3} lg={3} className="px-0">
-                <Card key={post.video.id} className={`${styles.card} mb-4`}>
-                  <video
-                    className="video"
-                    src={post.shortUrl}
-                    alt="wideo"
-                    controls="controls"
-                    style={{ width: "100%" }}
-                  />{" "}
-                </Card>
-              </Col>
-              <Col md={3} lg={3} className="px-0">
-                <Card className={`${styles.card} border-0`}>
-                  <CardHeader>
+              <div className="custom-card mb-4">
+                <video
+                  className="video"
+                  src={post.shortUrl}
+                  alt="wideo"
+                  controls
+                  style={{ width: "100%" }}
+                />
+              </div>
+            </div>
+            <div className="col-md-3 px-0">
+              <div className="custom-card border-0">
+                <div className="custom-card-header">
+                  <h2>
                     {post.user ? post.user.username : "Nieznany użytkownik"}
-                  </CardHeader>
-                  <Card.Title className="mb-0">{post.title}</Card.Title>
-                  <CardText className="mt-0">
-                    {" "}
-                    <small className="text-muted">{post.createTime}</small>
-                  </CardText>
-                </Card>
-              </Col>
-            </Row>
+                  </h2>
+                </div>
+                <h5 className="custom-card-title mb-0">{post.title}</h5>
+                <p className="custom-card-text mt-0">
+                  <small className="text-muted">{post.createTime}</small>
+                </p>
+              </div>
+            </div>
           </div>
         ))}
-    </Container>
+    </div>
   );
 }
 export default SearchScreen;
